669d6ce322b32331e189dc5de3fa8fb2db9e74a6
Mihaela Talpa <tal.mihaela@gmail.com>
2024-02-07 19:15:02 -0600

    adding updated int_contacts

--- northwinds_dbt/models/integration/int_contacts.sql
+++ northwinds_dbt/models/integration/int_contacts.sql
@@ -1,40 +1,17 @@
-WITH customers as(
-    SELECT * FROM {{ref('stg_rds_customers')}}
-), contacts as(
-    SELECT * FROM {{ref('stg_hubspot_contacts')}}
-),merged_contacts as(
-    SELECT
-    null as hubspot_contact_id,
-    customer_id as rds_contact_id,
-    first_name,
-    last_name,
-    phone,
-    null as hubspot_company_id,
-    company_id as rds_company_id
-    FROM customers
-    UNION ALL
-    SELECT
-    contact_id as hubspot_contact_id,
-    null as rds_contact_id,
-    first_name,
-    last_name,
-    phone,
-    company_id as hubspot_company_id,
-    null as rds_company_id
-    FROM contacts
-),final as(
-    SELECT
-    MAX(hubspot_contact_id) as hubspot_contact_id,
-    MAX(rds_contact_id) as rds_contact_id,
-    first_name,
-    last_name,
-    MAX(phone) as phone,
-    MAX(hubspot_company_id) as hubspot_company_id,
-    MAX(rds_company_id) as rds_company_id
-    FROM merged_contacts
-    GROUP BY first_name, last_name
-    ORDER BY last_name
-)
-select {{ dbt_utils.generate_surrogate_key(['first_name', 'last_name', 'phone']) }} as contact_pk,
-hubspot_contact_id, rds_contact_id,
-first_name, last_name, phone, hubspot_company_id, rds_company_id from final
+{% set sources = ["stg_hubspot_contacts", "stg_rds_customers"] %}
+
+  with merged_contacts as (
+    {% for source in sources %}
+        select first_name, {{ 'contact_id' if 'hubspot' in source else 'null' }} as hubspot_contact_id,
+        {{ 'customer_id' if 'rds' in source else 'null' }} as rds_contact_id
+         from {{ ref(source) }}
+        {% if not loop.last %}union all{% endif %}
+    {% endfor %}
+  ), 
+    deduped as (
+    select max(hubspot_contact_id) as hubspot_contact_id, 
+    max(rds_contact_id) as rds_contact_id, first_name from merged_contacts group by first_name
+  )
+  select {{ dbt_utils.generate_surrogate_key(['deduped.first_name']) }} as company_pk, hubspot_contact_id, rds_contact_id, deduped.first_name
+  from deduped
+  join {{ ref('stg_rds_customers') }} rds_companies on rds_companies.company_id = deduped.rds_contact_id
